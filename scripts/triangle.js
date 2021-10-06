9bd2b0c14d02d4d57038e430d34b059e6de28794
Al Hera Ahmad <alheraahmad0786@gmail.com>
2021-10-06 15:44:21 +0530

    fix : validation of angles

--- scripts/triangle.js
+++ scripts/triangle.js
@@ -6,13 +6,20 @@ function checkTriangle(){
     const angleOneNum = Number(angleInputs[0].value);
     const angleTwoNum = Number(angleInputs[1].value);
     const angleThreeNum = Number(angleInputs[2].value);
-    const sumOfAngles = angleOneNum + angleTwoNum + angleThreeNum ;
-    if(sumOfAngles === 180 ){
-        output.innerText = "This is a Triangle! ";
+
+    if(angleOneNum<=0 || angleTwoNum<=0 || angleThreeNum<=0){
+        output.innerText = "Please Enter valid angles ";
     }
     else{
-        output.innerText = "This is NOT a Triangle! ";
+      const sumOfAngles = angleOneNum + angleTwoNum + angleThreeNum ;
+        if(sumOfAngles === 180 ){
+            output.innerText = "This is a Triangle! ";
+        }
+        else{
+            output.innerText = "This is NOT a Triangle! ";
+        }
     }
+    
 }
 
 checkButton.addEventListener("click",checkTriangle);
